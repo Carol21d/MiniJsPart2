9a729eb5469d51bbcbe2028b1fde7514b56e0137
blake S <design.harls@icloud.com>
2022-11-22 12:10:43 +0100

    tests

---
+++ src/scripts/FizzBuzz.js
@@ -0,0 +1,15 @@
+export function FizzBuzz(numb){
+ if (numb % 5 == 0) {
+    return ("Buzz");
+ }
+}
+
+
+ export function fizzbuzz2(numb){
+    if (numb % 5 == 0 || numb % 3 == 0) {
+        return ("FizzBuzz");
+    }
+}
+
+
+
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,5 +1,53 @@
+import { FizzBuzz, fizzbuzz2 } from "../src/scripts/FizzBuzz";
+
 describe('FizzBuzz', () => {
-    test('should return Fizz', () => {
+    test('should return Buzz if is a divisible by 5', () => {
+
+        //Given
+        let numb = 5;
+
+        //When
+        const result = FizzBuzz(numb);
+
+
+        //Then
+        expect(result).toBe("Buzz");
         
     });
 });
+
+
+
+describe('FizzBuzz', () => {
+    test('should return FizzBuzz if is a divisible by 5 and by 3', () => {
+
+        //Given
+        let numb = 15;
+
+        //When
+        const result = fizzbuzz2(numb);
+
+
+        //Then
+        expect(result).toBe("FizzBuzz");
+        
+    });
+});
+
+
+
+// describe('FizzBuzz', () => {
+//     test('should return FizzBuzz if 1 to the 100', () => {
+
+//         //Given
+//         let numb;
+
+//         //When
+//         const result = fiz3(numb);
+
+
+//         //Then
+//         expect(result).toBe("FizzBuzz FizzBuzz");
+        
+//     });
+// });
