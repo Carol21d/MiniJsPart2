3f2f5e63ce4fc2fe1c7b145a6d6af8df13708b42
blake S <design.harls@icloud.com>
2022-12-30 18:06:41 +0100

    added function checkAge

--- src/scripts/Exercise1.js
+++ src/scripts/Exercise1.js
@@ -2,16 +2,22 @@ class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
-}
-    
-    dataPerson(){
-        console.log(`My name is ${this.name} and I'm ${this.age}`);
-    }
- 
+  }
+  dataPerson() {
+    console.log(` My name is ${this.name} and I'm ${(this, age)}`);
+  }
 }
 
-const name = prompt('Enter your name');
-const age = prompt('Enter your age');
+const name = prompt("Enter your name");
+const age = prompt("Enter your age");
 const person = new Person(name, age);
 person.dataPerson();
+checkAge(age);
 
+function checkAge(age) {
+  if (age > 18) {
+    console.log("Yes,IMaria  can drive");
+  } else {
+    console.log("I can't drive cause i'm under 18");
+  }
+}
